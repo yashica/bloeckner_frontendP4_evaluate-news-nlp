9b316ba695c0ca3ce7604da21fe5a1e6bda6b711
Anna Magdalena Bloeckner <m.bloeckner@web.de>
2020-04-13 12:58:54 +0200

    add urlChecker.js, reorder server file

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+import { checkForUrl } from "./urlChecker";
+
 function handleSubmit(event) {
   event.preventDefault();
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,16 @@
+//setup use of Environment Variables
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Empty JS object to act as endpoint for all routes
+let projectData = {};
+
+/* Dependencies */
 var path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const mockAPIResponse = require("./mockAPI.js");
-
 //Require the Aylien npm package
 var AYLIENTextAPI = require("aylien_textapi");
 
@@ -14,28 +20,37 @@ var textapi = new AYLIENTextAPI({
   application_key: process.env.API_KEY, //"1404bb62f2a70203aeec19aec6f890dd", //`${process.env.application_key}`,
 });
 
+/* Start up an instance of app */
 const app = express();
 
-app.use(express.static("dist"));
+/* Middleware*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
+/* Initialize the main project folder*/
+app.use(express.static("dist"));
 console.log(__dirname);
 
-app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
-});
-
+/* Spin up the server*/
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
   console.log("Example app listening on port 8081!");
+});
+
+console.log("TEST TEST TEST");
+
+/* routes */
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 
-console.log("TEST TEST TEST");
-
+//aylien test
 textapi.sentiment(
   {
     text: "John is a very good football player!",
